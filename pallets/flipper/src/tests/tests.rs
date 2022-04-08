1561494b2403dfbd58c50ebc42220211d38b0d56
nathan gd <nathan.gardet-derc@pm.me>
2022-04-08 15:30:18 +0200

    exercice completed.

--- pallets/flipper/src/tests/tests.rs
+++ pallets/flipper/src/tests/tests.rs
@@ -5,20 +5,38 @@ use frame_support::{assert_noop, assert_ok};
 #[test]
 fn set_value_ok() {
 	new_test_ext().execute_with(|| {
-		// TODO: ensure the good behaviour of set_value() function.
+		assert_ok!(FlipperModule::set_value(Origin::signed(1), true));
+		assert_eq!(FlipperModule::value(), Some(true));
 	});
 }
 
 #[test]
 fn set_value_err_already_set() {
 	new_test_ext().execute_with(|| {
-		// TODO: verify if the function returns the expected error.
+		assert_ok!(FlipperModule::set_value(Origin::signed(1), true));
+		assert_noop!(
+			FlipperModule::set_value(Origin::signed(1), true),
+			Error::<Test>::AlreadySet
+		);
 	});
 }
 
 #[test]
 fn flip_value_ok() {
-	// TODO: ensure the good behaviour of flip_value() function.
+	new_test_ext().execute_with(|| {
+		assert_ok!(FlipperModule::set_value(Origin::signed(1), true));
+		assert_ok!(FlipperModule::flip_value(Origin::signed(1)));
+
+		assert_eq!(FlipperModule::value(), Some(false))
+	})
 }
 
-// TODO: Make another test to check the behaviour in the case where an error occured in the flip_function().
+#[test]
+fn flip_value_no_value() {
+	new_test_ext().execute_with(|| {
+		assert_noop!(
+			FlipperModule::flip_value(Origin::signed(1)),
+			Error::<Test>::NoneValue
+		);
+	})
+}
